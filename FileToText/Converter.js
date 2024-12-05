c478718519961b6da90ae367c8504e1df802396f
Aaditya <aaditya.patil@walchandsangli.ac.in>
2024-12-05 22:14:14 +0530

    feat:added reward penalty

--- FileToText/Converter.js
+++ FileToText/Converter.js
@@ -1,17 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const pdf = require('pdf-parse');
+const fs = require("fs");
+const path = require("path");
+const pdf = require("pdf-parse");
 
 async function convertPDFToText(inputFilePath, outputTextFilePath) {
-    const dataBuffer = fs.readFileSync(inputFilePath);
-    const data = await pdf(dataBuffer);
-    const text = data.text;
-    await fs.promises.writeFile(outputTextFilePath, text);
-    return text;
+  const dataBuffer = fs.readFileSync(inputFilePath);
+  const data = await pdf(dataBuffer);
+  const text = data.text;
+  await fs.promises.writeFile(outputTextFilePath, text);
+  return text;
 }
 
-
-
 module.exports = {
-    convertPDFToText,
-};  
+  convertPDFToText,
+};
